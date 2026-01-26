89d0d3cbbc2861bd733bda2fbbd7d81ce3492423
Igorechkere <igorshvedenko@yandex.ru>
2026-01-26 17:03:44 +0300

    add html and styles

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-try {
-    const prom = await fetch('https://goweather.xyz/weather/London');
-    const finalprom = await prom.json();
-    console.log(finalprom) 
-} catch(err) {
-    console.log('Ошибка', err)
-}
+// try {
+//     const prom = await fetch('https://goweather.herokuapp.com/weather/Curitiba');
+//     const finalprom = await prom.json();
+//     console.log(finalprom) 
+// } catch(err) {
+//     console.log('Ошибка', err)
+// }
 
-// варианты: ['Haze', 'Patchy rain nearby', 'Clear ', 'Partly Cloudy ', 
+// варианты: ['Haze', 'Patchy rain nearby', 'Clear ', 'Partly cloudy ', 
 // 'Mist', 'Overcast ', 'Cloudy ', 'Light drizzle', 'Light rain', 
 // 'Moderate rain', 'Sunny', 'Cloudy', 'Freezing fog', 'Heavy snow', 
 // 'Moderate snow', 'Light snow', 'Light freezing rain', 'Light rain shower', 
-// 'Partly cloudy']
+// 'Partly cloudy', 'Freezing drizzle']

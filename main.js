bf076b40aa5d207c0b19d05104ae7364633ad0b8
Igorechkere <igorshvedenko@yandex.ru>
2026-01-27 17:39:50 +0300

    Version 1.0

--- main.js
+++ main.js
@@ -5,18 +5,58 @@ const speed = document.getElementById('speedWind');
 const humproc = document.getElementById('humproc');
 const day = document.getElementById('day');
 const dayWeek = document.getElementById('dayWeek');
-const time = document.getElementById('time')
+const time = document.getElementById('time');
+const wph = document.getElementById('weathPhoto');
+const nameW = document.getElementById('weatherName');
 search.addEventListener('click', async (event) => {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const sunny = ['clear', 'overcast' , 'sunny'];
+    const cloudy = ['cloudy' , 'partly cloudy'];
+    const rainy = ['patchy rain nearby', 'light drizzle', 'light rain', 'moderate rain', 'light freezing rain', 'light rain shower', 'rain', 'partly cloudy'];
+    const foggy = ['haze', 'mist', 'freezing fog'];
+    const snow = ['heavy snow', 'moderate snow', 'light snow', 'freezing drizzle', 'snow', 'light snow shower']
     const city = input.value;
     if (city === '' || city === undefined) {
         alert('Введите название города')
         return
     }
+    wph.setAttribute('src', 'photo/download.gif');
+    wph.setAttribute('height', '64px');
+    wph.setAttribute('width', '64px');
     const info = await getFetch(city);
+    const description = info.description.toLowerCase();
+    if (sunny.includes(description)) {
+        wph.setAttribute('src', 'photo/icons/Sunny.png');
+        wph.setAttribute('width', '64px');
+        wph.setAttribute('height', '64px');
+        nameW.textContent = 'Sunny';
+    } else if (rainy.includes(description)) {
+        wph.setAttribute('src', 'photo/icons/Rainy1.png');
+        wph.setAttribute('width', '102px');
+        wph.setAttribute('height', '74px');
+        nameW.textContent = 'Rainy';
+    } else if (cloudy.includes(description)) {
+        wph.setAttribute('src', 'photo/icons/Cloudy.png');
+        wph.setAttribute('width', '102px');
+        wph.setAttribute('height', '74px');
+        nameW.textContent = 'Cloudy';
+    } else if (foggy.includes(description)) {
+        wph.setAttribute('src', 'photo/icons/Fog.png');
+        wph.setAttribute('width', '102px');
+        wph.setAttribute('height', '74px');
+        nameW.textContent = 'Fog';
+    } else if (snow.includes(description)) {
+        wph.setAttribute('src', 'photo/icons/Snow.png');
+        wph.setAttribute('width', '102px');
+        wph.setAttribute('height', '74px');
+        nameW.textContent = 'Snow';
+    } else {
+        wph.setAttribute('src', 'photo/cloudy.png');
+    }
+    console.log(description);
     grad.textContent = info.temperature;
-    speed.textContent = info.wind;
-    humproc.textContent = `${Math.trunc(Math.random()*100)}%`
+    speed.textContent = `Wind: ${info.wind}`;
+    humproc.textContent = `Hum: ${Math.trunc(Math.random()*100)}%`
     console.log(info);
     const date = new Date();
     dayWeek.textContent = `${days[date.getDay() - 1]}`
@@ -37,4 +77,4 @@ async function getFetch(city) {
 // 'Mist', 'Overcast ', 'Cloudy ', 'Light drizzle', 'Light rain', 
 // 'Moderate rain', 'Sunny', 'Cloudy', 'Freezing fog', 'Heavy snow', 
 // 'Moderate snow', 'Light snow', 'Light freezing rain', 'Light rain shower', 
-// 'Partly cloudy', 'Freezing drizzle']
+// 'Partly cloudy', 'Freezing drizzle', 'rain']
